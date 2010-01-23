a36360c61be8619522695a9320a1b2074fdab58d
Ryan Davis <ryand@zenspider.com>
2010-01-22 16:32:49 -0800

    - Fixed failed tests for new assert_same output
    [git-p4: depot-paths = "//src/minitest_tu_shim/dev/": change = 5613]

--- test/test_mini_test.rb
+++ test/test_mini_test.rb
@@ -538,16 +538,16 @@ Expected [RuntimeError] to include SyntaxError."
   end
 
   def test_assert_same_triggered
-    @assertion_count = 2
-
-    util_assert_triggered 'Expected 2 (0xXXX) to be the same as 1 (0xXXX).' do
+    util_assert_triggered 'Expected 2 (oid=N) to be the same as 1 (oid=N).' do
       @tc.assert_same 1, 2
     end
+  end
 
+  def test_assert_same_equal_triggered
     s1 = "blah"
     s2 = "blah"
 
-    util_assert_triggered 'Expected "blah" (0xXXX) to be the same as "blah" (0xXXX).' do
+    util_assert_triggered 'Expected "blah" (oid=N) to be the same as "blah" (oid=N).' do
       @tc.assert_same s1, s2
     end
   end
@@ -822,7 +822,7 @@ Expected [RuntimeError] to include SyntaxError."
     end
 
     msg = e.message.sub(/(---Backtrace---).*/m, '\1')
-    msg.gsub!(/\(0x[0-9a-f]+\)/, '(0xXXX)')
+    msg.gsub!(/\(oid=[-0-9]+\)/, '(oid=N)')
 
     assert_equal expected, msg
   end
